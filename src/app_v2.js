7d9e3d91767e840eee8c9caafb1fd302bc39fe9b
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-19 18:22:59 +0200

    añadir app_v2.js y renombrar app.js a app_v1.js

---
+++ src/app_v2.js
@@ -0,0 +1,8 @@
+// Piedra, papel, tijera, lagarto, spock
+/*
+spock, papel    > piedra  > tijera, lagarto
+tijera, lagarto > papel   > piedra, spock
+spock, piedra   > tijera  > papel, lagarto
+piedra, tijera  > lagarto > spock, papel
+lagarto, papel  > spock   > tijera, piedra
+*/
