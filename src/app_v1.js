bd81cf3c151af1a573ba02599d13409a8a4c8c1c
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-19 20:27:39 +0200

    funcion para cambiar imagen de la ficha segun seleccion del jugador

--- src/app_v1.js
+++ src/app_v1.js
@@ -8,12 +8,20 @@ lagarto, papel  > spock   > tijera, piedra
 */
 const imagenes = ["./assets/rock.png", "./assets/paper.png", "./assets/scissors.png", "./assets/lizard.png", "./assets/spock.png"];
 const contenedorImagenCPU = document.querySelector("#vsCPU");
+const contenerdoImagenPlayer = document.querySelector("#vsPlayer");
 const indicador = document.querySelector("#indicador");
 let count = 0;
 let intervalo1;
 
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
+const lizardBtn = document.querySelector("#lizard-btn");
+const spockBtn = document.querySelector("#spock-btn");
+
 const fichas = ["piedra", "papel", "tijera", "lagarto", "spock"];
 
+//LÓGICA DEL JUEGO
 const pptls = (eleccion, vs) => {
   console.log(`${eleccion} VS ${vs}!!!!!`);
   if (vs === eleccion) { return "Empate!" };
@@ -76,6 +84,34 @@ const imgSwitch = (arr) => {
   }
 }
 
+// FUNCIÓN PARA CAMBIAR LA IMAGEN EN FUNCIÓN DE LA SELECCIÓN
+const playerImg = (valor) => {
+  contenerdoImagenPlayer.innerHTML = `
+    <img src="${imagenes[valor]}" class="" width="250" alt="">`;
+}
+
+rockBtn.addEventListener("change", () => {
+  console.log("Has seleccionado Piedra");
+  playerImg(rockBtn.value);
+});
+paperBtn.addEventListener("change", () => {
+  console.log("Has seleccionado Papel");
+  playerImg(paperBtn.value);
+});
+scissorsBtn.addEventListener("change", () => {
+  console.log(`Has seleccionado Tijeras`);
+  playerImg(scissorsBtn.value);
+});
+lizardBtn.addEventListener("change", () => {
+  console.log("Has seleccionado Lagarto");
+  playerImg(lizardBtn.value);
+});
+spockBtn.addEventListener("change", () => {
+  console.log("Has seleccionado Spock");
+  playerImg(spockBtn.value);
+});
+
+// EJECUTA AL CARGAR LA PÁGINA
 window.onload = () => {
   intervalo1 = setInterval(() => { imgSwitch(imagenes) }, 500);
   setTimeout(() => { clearInterval(intervalo1) }, 10000);
