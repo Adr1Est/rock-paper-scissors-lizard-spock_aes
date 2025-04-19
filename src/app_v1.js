ef8c29dcde5e35e129c9714c2594dee9330546d4
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-19 20:12:01 +0200

    frontend y funcion intervalo para imagen cpu player

--- src/app_v1.js
+++ src/app_v1.js
@@ -6,6 +6,11 @@ spock, piedra   > tijera  > papel, lagarto
 piedra, tijera  > lagarto > spock, papel
 lagarto, papel  > spock   > tijera, piedra
 */
+const imagenes = ["./assets/rock.png", "./assets/paper.png", "./assets/scissors.png", "./assets/lizard.png", "./assets/spock.png"];
+const contenedorImagenCPU = document.querySelector("#vsCPU");
+const indicador = document.querySelector("#indicador");
+let count = 0;
+let intervalo1;
 
 const fichas = ["piedra", "papel", "tijera", "lagarto", "spock"];
 
@@ -54,9 +59,26 @@ const pptls = (eleccion, vs) => {
   }
 }
 
+//JUGADA ALEATORIA - CPU PLAYER
 function randomArrElement(arr) {
   let jugada = arr[Math.floor(Math.random() * arr.length)]
   return jugada;
+}
+
+//CAMBIO DE IMÁGENES
+const imgSwitch = (arr) => {
+  if (count === arr.length) { return count = 0; }
+  if (count < arr.length) {
+    contenedorImagenCPU.innerHTML = `
+      <img src="${arr[count]}" class="" width="250" alt="">`;
+    console.log(count);
+    return count++;
+  }
+}
+
+window.onload = () => {
+  intervalo1 = setInterval(() => { imgSwitch(imagenes) }, 500);
+  setTimeout(() => { clearInterval(intervalo1) }, 10000);
 }
 
 console.log(pptls(randomArrElement(fichas), randomArrElement(fichas)));
